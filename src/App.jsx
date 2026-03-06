44711da2073125113d8cc8b70df0449526c0cd52
肉球 <as2711830@gmail.com>
2026-03-06 18:51:35 +0800

    Day 1：建立第一個 React 專案

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>我的第一個 React 專案</h1>
+    </div>
+  );
+}
+
+export default App;
