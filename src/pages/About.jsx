dede6bf0af6ef0124d8e7f8a942b6adbbc7e2851
肉球 <as2711830@gmail.com>
2026-03-09 15:27:37 +0800

    Day 15: useContext 基礎，createContext、Provider、useContext 用法

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,16 @@
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+
 // src/pages/About.jsx
 function About() {
-  return <h1>關於我</h1>;
+  const user = useContext(UserContext);
+  console.log(UserContext);
+  return (
+    <div>
+      <h1>關於我</h1>
+      <p>使用者：{user.name}</p>
+      <p>身份：{user.role}</p>
+    </div>
+  );
 }
 export default About;
